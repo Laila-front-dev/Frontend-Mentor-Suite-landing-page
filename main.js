41f75255aa66c1607d0e39c9189e93636b5e0fd8
Laila-front-dev <your.lailaben@gmail.com>
2024-11-17 20:41:28 +0100

    update file

---
+++ main.js
@@ -0,0 +1,32 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Scroll Number
+
+let nums = document.querySelectorAll(".num");
+
+nums.forEach((num) => startCount(num));
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 150);
+}
+
+startCount(document.querySelectorAll(".num"));
